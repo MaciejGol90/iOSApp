ffb5f665424248157779296678cdec4e086d67b2
Maciej Golianek <maciek.golianek@gmail.com>
2020-01-13 20:10:01 +0100

    first commit

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Maciej Golianek on 10/01/2020.
+//  Copyright © 2020 Maciej Golianek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
