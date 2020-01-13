f2b505e3768372f5d58ef93fbc3093d3978d3c2e
Maciej Golianek <maciek.golianek@gmail.com>
2020-01-13 19:46:40 +0100

    Initial Commit

---
+++ MyApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyApp
+//
+//  Created by Maciej Golianek on 13/01/2020.
+//  Copyright © 2020 Maciej Golianek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
