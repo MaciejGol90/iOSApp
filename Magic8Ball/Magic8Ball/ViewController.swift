33bf315aa651f9dd0a2e6bd19123bd09b6692b9f
Maciej Golianek <maciek.golianek@gmail.com>
2020-01-14 18:30:38 +0100

    add

--- Magic8Ball/Magic8Ball/ViewController.swift
+++ Magic8Ball/Magic8Ball/ViewController.swift
@@ -11,7 +11,9 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var answerImage: UIImageView!
-    @IBAction func askButton(_ sender: UIButton) {
+    
+    
+    @IBAction func przyciskPytajacy(_ sender: UIButton) {
         let answerOptionImage = [#imageLiteral(resourceName: "ball1"), #imageLiteral(resourceName: "ball2"), #imageLiteral(resourceName: "ball3"), #imageLiteral(resourceName: "ball4"), #imageLiteral(resourceName: "ball5")]
        
         answerImage.image = answerOptionImage[Int.random(in: 0...4)]
