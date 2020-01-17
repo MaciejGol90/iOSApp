76cf2f449606573c65c8415e11e7632c9b5310eb
Maciej Golianek <maciek.golianek@gmail.com>
2020-01-17 09:00:13 +0100

    xylophoneInitProject

---
+++ Xylophone/Xylophone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Maciej Golianek on 17/01/2020.
+//  Copyright © 2020 Maciej Golianek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
