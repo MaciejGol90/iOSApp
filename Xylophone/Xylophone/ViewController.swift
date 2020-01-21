6ac23fac973f0a93fa45c630188731f10626d2eb
Maciej Golianek <maciek.golianek@gmail.com>
2020-01-21 18:23:31 +0100

    add working app

--- Xylophone/Xylophone/ViewController.swift
+++ Xylophone/Xylophone/ViewController.swift
@@ -16,17 +16,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    func playSound() {
-           let url = Bundle.main.url(forResource: "C", withExtension: "wav")
-           player = try! AVAudioPlayer(contentsOf: url!)
-           player.play()
-                   
-       }
+
     @IBAction func keyPressed(_ sender: UIButton) {
         playSound()
     }
     
-   
+    func playSound() {
+        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
 
 
