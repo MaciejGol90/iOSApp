b81c8ec9b7fc333224ea735da004fae128da2f0e
Maciej Golianek <maciek.golianek@gmail.com>
2020-01-25 17:59:57 +0100

    eggTimerInit

--- Xylophone 2AppBewery/Xylophone/ViewController.swift
+++ Xylophone 2AppBewery/Xylophone/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController {
     
         playSound(soundName: sender.currentTitle!)
         sender.alpha = 0.5
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
             sender.alpha = 1
         }
     }
