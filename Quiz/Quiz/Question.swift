d43beaa3d0b199cabdb5a74ded34d77169814fb6
Maciej Golianek <maciek.golianek@gmail.com>
2020-02-10 19:56:24 +0100

    addQuestion&ColorButtonQuize

--- Quiz/Quiz/Question.swift
+++ Quiz/Quiz/Question.swift
@@ -11,4 +11,9 @@ struct Question {
     let text: String
     let answer: String
     
+    init(q: String, a: String) {
+        text = q
+        answer = a
+    }
+    
 }
--- Quiz/Quiz/ViewController.swift
+++ Quiz/Quiz/ViewController.swift
@@ -14,9 +14,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
     let quiz = [
-        Question (text: "2+4=6?", answer: "TAK"),
-        Question (text: "Pierwiastek z 9 to 3?", answer: "TAK"),
-        Question (text: "Czy można dzielić przez 0?", answer: "NIE")
+        Question(q: "2+4=6?", a: "TAK"),
+        Question(q: "Pierwiastek z 9 to 3?", a: "TAK"),
+        Question(q: "Czy można dzielić przez 0?", a: "NIE"),
+        Question(q: "Czy słoń jest w stanie z sienie wydać dzwięk głośności 188 decybeli", a: "TAK"),
+        Question(q: "Czy ryś jest kotem?", a: "TAK"),
+        Question(q: "Czy antylopa jest szybsza niż gepard?", a: "NIE"),
+        Question(q: "Czy Sylwek i Sylwester to to samo?", a: "TAK"),
+        Question(q: "Czy w widzie jest więcej tleny niż wodoru?", a: "TAK"),
+        Question(q: "Czy niebo jest zielone?", a: "NIE"),
+        Question(q: "Czy stropian jest jadalny?", a: "NIE"),
+        Question(q: "Cynamon to kora?", a: "TAK"),
+        Question(q: "Czy światło może mieć różną barwę?" , a: "TAK")
         ]
     var questionNumber = 0
     override func viewDidLoad() {
@@ -28,20 +37,26 @@ class ViewController: UIViewController {
         let userAnserw = sender.currentTitle
         let acualAnswer = quiz[questionNumber].answer
         if userAnserw == acualAnswer{
+            sender.backgroundColor = UIColor.green
             print("Brawo!!! Świetnie Mordeczko")
+          
         }else{
+            sender.backgroundColor = UIColor.red
             print("Nie tym razem Mordo!")
+          
         }
        if questionNumber + 1 < quiz.count{
         questionNumber += 1
        }else{
         questionNumber = 0
        }
-        updateUI ()
+        let timer = Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
         }
         
-    func updateUI() {
+    @objc func updateUI() {
         questionText.text = quiz[questionNumber].text
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear 
              }
-}
+    }
 
