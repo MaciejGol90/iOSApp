6866561a14bf19c7ba4719c4ffcfe8388adb28ab
Maciej Golianek <maciek.golianek@gmail.com>
2020-02-11 06:58:24 +0100

    addProgressBarQuize

--- Quiz/Quiz/ViewController.swift
+++ Quiz/Quiz/ViewController.swift
@@ -17,11 +17,11 @@ class ViewController: UIViewController {
         Question(q: "2+4=6?", a: "TAK"),
         Question(q: "Pierwiastek z 9 to 3?", a: "TAK"),
         Question(q: "Czy można dzielić przez 0?", a: "NIE"),
-        Question(q: "Czy słoń jest w stanie z sienie wydać dzwięk głośności 188 decybeli", a: "TAK"),
+        Question(q: "Czy słoń jest w stanie z siebie wydać dzwięk głośności 188 decybeli", a: "TAK"),
         Question(q: "Czy ryś jest kotem?", a: "TAK"),
         Question(q: "Czy antylopa jest szybsza niż gepard?", a: "NIE"),
         Question(q: "Czy Sylwek i Sylwester to to samo?", a: "TAK"),
-        Question(q: "Czy w widzie jest więcej tleny niż wodoru?", a: "TAK"),
+        Question(q: "Czy w wodzie jest więcej tleny niż wodoru?", a: "TAK"),
         Question(q: "Czy niebo jest zielone?", a: "NIE"),
         Question(q: "Czy stropian jest jadalny?", a: "NIE"),
         Question(q: "Cynamon to kora?", a: "TAK"),
@@ -47,16 +47,18 @@ class ViewController: UIViewController {
         }
        if questionNumber + 1 < quiz.count{
         questionNumber += 1
+        
        }else{
         questionNumber = 0
        }
-        let timer = Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+        Timer.scheduledTimer(timeInterval: 0.3, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
         }
         
     @objc func updateUI() {
         questionText.text = quiz[questionNumber].text
         trueButton.backgroundColor = UIColor.clear
-        falseButton.backgroundColor = UIColor.clear 
-             }
+        falseButton.backgroundColor = UIColor.clear
+        progressBar.progress = Float(questionNumber + 1) / Float(quiz.count)
+             
     }
-
+}
