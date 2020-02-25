42d4f7f5b6a568ffe9dc25c0c914b1893ca3203a
Maciej Golianek <maciek.golianek@gmail.com>
2020-02-25 18:59:18 +0100

    destinyInit

---
+++ Przeznaczenie/Przeznaczenie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Przeznaczenie
+//
+//  Created by Maciej Golianek on 25/02/2020.
+//  Copyright © 2020 Maciej Golianek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
